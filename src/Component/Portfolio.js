4f6f788302b08cdb328013e2652dac77595cabc5
Khawar Hayat <khawarhayat1997@gmail.com>
2020-08-19 14:55:45 +0500

    V 2.3

--- src/Component/Portfolio.js
+++ src/Component/Portfolio.js
@@ -7,7 +7,7 @@ class Portfolio extends Component {
             <div id='Portfolio'>
             <center><h2>Portfolio</h2></center>
             <div className={style.main}>
-            <ProjectBox src='Portfolio.jpg' projectName='Portfolio 2.0' list={['React', 'CSS3', 'JavaScript']} codeLink='https://github.com/KhawarHayat/Portfolio' siteLink='https://github.com/KhawarHayat/Portfolio' />
+            <ProjectBox src='Portfolio.jpg' projectName='Portfolio 2.0' list={['React', 'CSS3', 'JavaScript']} codeLink='https://github.com/KhawarHayat/Portfolio' siteLink='https://master.d3iivkwy7rqvu6.amplifyapp.com/' />
             <ProjectBox src='MilkMan.jpeg' projectName='Milk Man' list={['React', 'Redux', 'Semantic-UI', 'Node', 'Express.js', 'Restful APIs', 'MongoDB']} codeLink='https://github.com/KhawarHayat/MilkMan' />
             <ProjectBox src='FMS.jpeg' projectName='Frequency Management System' list={['React', 'Semantic-UI', 'Node', 'Express.js', 'Restful APIs', 'MySQL']} codeLink='https://github.com/KhawarHayat/FreqManagement' />
             <ProjectBox src='Portfolio.jpeg' projectName='Portfolio 1.0' list={['Next.js', 'Style Component', 'JavaScript']} codeLink='https://github.com/KhawarHayat/Portfolio' />
